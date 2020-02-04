542de672fe38bff29841668e2bffed250b2534a0
vinothkumar <net.vinothkumar@gmail.com>
2020-02-04 13:12:50 +0100

    monitoring

---
+++ src/test/java/com/monitoring/prometheus/PrometheusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monitoring.prometheus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrometheusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
