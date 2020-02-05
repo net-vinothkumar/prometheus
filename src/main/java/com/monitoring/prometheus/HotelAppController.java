573ce5efb9bc3a4a84a200eedfbefa87745e2384
vinothkumar <net.vinothkumar@gmail.com>
2020-02-05 07:08:47 +0100

    comments

--- src/main/java/com/monitoring/prometheus/HotelAppController.java
+++ src/main/java/com/monitoring/prometheus/HotelAppController.java
@@ -1,13 +1,32 @@
 package com.monitoring.prometheus;
 
+import io.micrometer.core.annotation.Timed;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HotelAppController {
 
+    // collects the timely information about this endpoint
+    @Timed(
+            value = "monitoring.hotels.request",
+            histogram = true,
+            percentiles = {0.95, 0.99},
+            extraTags = {"version", "1.0"}
+    )
     @GetMapping("/hotels")
     public String getHotel() {
         return "Hotel Taj";
+    }
+
+    @Timed(
+            value = "monitoring.restaurants.request",
+            histogram = true,
+            percentiles = {0.95, 0.99},
+            extraTags = {"version", "1.0"}
+    )
+    @GetMapping("/restaurants")
+    public String getRestaurant() {
+        return "Empire Restaurant";
     }
 }
--- src/main/java/com/monitoring/prometheus/PrometheusApplication.java
+++ src/main/java/com/monitoring/prometheus/PrometheusApplication.java
@@ -1,13 +1,24 @@
 package com.monitoring.prometheus;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.metrics.MeterRegistryCustomizer;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PrometheusApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PrometheusApplication.class, args);
+	}
+
+	@Bean
+	MeterRegistryCustomizer meterRegistryCustomizer(MeterRegistry meterRegistry) {
+		return meterRegistry1 -> {
+			meterRegistry.config()
+					.commonTags("application", "micrometer-monitoring-example");
+		};
 	}
 
 }
